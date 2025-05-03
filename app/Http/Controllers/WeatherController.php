20b30862c985f9f6f0002680e481e8ecb0d53cbe
joykara <joykarani1@gmail.com>
2025-05-03 11:24:27 +0300

    refactor: group forecast data by day

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class WeatherController extends Controller
 {
@@ -31,7 +33,6 @@ class WeatherController extends Controller
             'lon' => $lon,
             'appid' => $apiKey,
             'units' => 'metric',
-            'cnt' => 4,
         ]);
 
         // Handle failed API responses
@@ -43,12 +44,57 @@ class WeatherController extends Controller
             ], 500);
         }
 
-        // Combine the datae
+        $forecastData = $forecastResponse->json()['list'];
+
+        // Group forecast by day
+        $dailyForecasts = [];
+
+        foreach ($forecastData as $entry) {
+            $date = Carbon::parse($entry['dt_txt'])->format('Y-m-d');
+
+            // Skip today
+            if ($date === Carbon::today()->format('Y-m-d')) {
+                continue;
+            }
+
+            if (!isset($dailyForecasts[$date])) {
+                $dailyForecasts[$date] = [
+                    'temps' => [],
+                    'descriptions' => [],
+                    'icons' => [],
+                ];
+            }
+
+            $dailyForecasts[$date]['temps'][] = $entry['main']['temp'];
+            $dailyForecasts[$date]['descriptions'][] = $entry['weather'][0]['description'];
+            $dailyForecasts[$date]['icons'][] = $entry['weather'][0]['icon'];
+        }
+
+        // Next 3 days
+        $summary = [];
+        foreach (array_slice($dailyForecasts, 0, 3) as $date => $info) {
+            $summary[] = [
+                'date' => $date,
+                'min_temp' => round(min($info['temps']), 1),
+                'max_temp' => round(max($info['temps']), 1),
+                'description' => ucfirst(mostFrequent($info['descriptions'])),
+                'icon' => $info['icons'][0],
+            ];
+        }
+
+        // Combine the data
         $data = [
             'current' => $currentWeatherResponse->json(),
-            'forecast' => $forecastResponse->json()['list'],
+            'forecast' => $summary,
         ];
 
         return response()->json($data);
     }
+}
+
+function mostFrequent(array $array): string
+{
+    $counts = array_count_values($array);
+    arsort($counts);
+    return array_key_first($counts);
 }
